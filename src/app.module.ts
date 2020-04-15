28b4840a3754e9eb20264529eaa9d3e7dd894de6
Spencer <spencer@sana.com.au>
2020-04-15 21:02:17 +1000

    Make the api gateway work for invoices routes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common'
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  HttpModule
+} from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ReverseProxyMiddleware } from './reverseProxy.middleware'
+import { VerifyTokenMiddleware } from './verifyToken.middleware'
+import { InvoicesModule } from './invoices/invoices.module'
 
 @Module({
-  imports: [],
+  imports: [InvoicesModule, HttpModule],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(VerifyTokenMiddleware, ReverseProxyMiddleware)
+      .forRoutes('invoices')
+  }
+}
---
+++ src/invoices/invoices.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common'
+import { InvoicesController } from './invoices.controller'
+
+@Module({
+  imports: [],
+  controllers: [InvoicesController]
+})
+export class InvoicesModule {}
